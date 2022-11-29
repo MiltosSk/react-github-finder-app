e295e1c836aee5fa61e3ba93a1416ac5de949146
Miltos Skondras <miltiadis.skondras.2018@gmail.com>
2022-11-29 23:22:19 +0000

    Adding Search User functionality

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -4,7 +4,7 @@ import GithubContext from "../../context/github/GithubContext"
 function UserSearch() {
     const [text, setText] = useState('')
 
-    const {users} = useContext(GithubContext)
+    const {users, searchUsers} = useContext(GithubContext)
 
     //Sets text to whatever is typed in
     const handleChange = (e) => setText(e.target.value)
@@ -15,7 +15,7 @@ function UserSearch() {
         if(text === ''){
             alert('Please enter something')
         } else {
-            //@todo - search users
+            searchUsers(text)
 
             setText('')
         }
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer, useState } from "react";
+import { createContext, useReducer, } from "react";
 import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext()
@@ -13,21 +13,26 @@ export const GithubProvider = ({children}) => {
     }
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
-    // Gets initial users (used for testing purposes)
-    const fetchUsers = async () => {
+    // Gets search results
+    const searchUsers = async (text) => {
         setLoading()
-        const response = await fetch(`${GITHUB_URL}/users`,
+
+        const params = new URLSearchParams({
+            q: text
+        })
+
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`,
             {
                 headers: {
                     Authorization: `token ${GITHUB_TOKEN}`
                 }
             }
         )
-        const data = await response.json()
+        const {items} = await response.json()
         
         dispatch({
             type: 'GET_USERS',
-            payload: data,
+            payload: items,
         })
     }
 
@@ -39,7 +44,7 @@ export const GithubProvider = ({children}) => {
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
     }}>
         {children}
     </GithubContext.Provider>
