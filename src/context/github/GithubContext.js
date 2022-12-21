eab20853349ca69a58d536bcf2d78b0b730efd9e
Miltos Skondras <miltiadis.skondras.2018@gmail.com>
2022-12-21 22:29:33 +0200

    Getting User Repos

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
 import { createContext, useReducer, } from "react";
+import { createRenderer } from "react-dom/test-utils";
 import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext()
@@ -10,6 +11,7 @@ export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
         user: {},
+        repos: [],
         loading: false
     }
 
@@ -59,8 +61,30 @@ export const GithubProvider = ({children}) => {
                 payload: data,
             })
         }
+    }
 
-       
+    // Gets user repos
+    const getUserRepos = async (login) => {
+        setLoading()
+
+        const params = new URLSearchParams({
+            sort: 'created',
+            per_page: 10
+        })
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`,
+            {
+                headers: {
+                    Authorization: `token ${GITHUB_TOKEN}`
+                }
+            }
+        )
+        const data = await response.json()
+        
+        dispatch({
+            type: 'GET_REPOS',
+            payload: data,
+        })
     }
 
     //Clear users from state
@@ -77,9 +101,11 @@ export const GithubProvider = ({children}) => {
         users: state.users,
         loading: state.loading,
         user: state.user,
+        repos: state.repos,
         searchUsers,
         clearUsers,
         getUser,
+        getUserRepos
     }}>
         {children}
     </GithubContext.Provider>
